4b88bc0e910db0d92c7e5f0409acb691624eaf44
Korkmatik <9c54efc1-143b-4f5f-9d2f-f6f189583f0f@aleeas.com>
2021-04-11 22:05:33 +0200

    Add Service Registry

---
+++ service-registry/src/test/java/korkmatik/irem/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package korkmatik.irem.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
